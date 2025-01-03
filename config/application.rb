b05715c13be87aa2cf7e5a969adffcc70dda555b
Diego-S-G <diego07goncalves@gmail.com>
2025-01-03 13:23:26 -0300

    Setting up the API Client.

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module TaskManagerApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.api_only = true # quando rodarmos scaffold ele vai nos dar apenas o que precisamos para uma API
+
+    # aqui ele removeu dps que fez o scaffold mas vou deixar aqui comentado só pra ter
+    # config.api_only = true # quando rodarmos scaffold ele vai nos dar apenas o que precisamos para uma API
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
